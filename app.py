dfed18c5ce49c968119e5e395138ec9be8587256
Tyler Hallada <tyler@hallada.net>
2015-08-02 22:31:36 -0400

    Make the haiku-generator part of greater nlp app

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from generate_poem import PoemGenerator
 app = Flask(__name__)
 
 generator = PoemGenerator(None)
 
+
 @app.route("/")
 def home():
+    return redirect('http://git.hallada.net/nlp/')
+
+
+@app.route("/buzzfeed-haiku-generator")
+def buzzfeed_haiku_generator():
     haiku = generator.generate_haiku()
-    return render_template('index.html', haiku=haiku)
+    return render_template('buzzfeed-haiku-generator.html', haiku=haiku)
 
 if __name__ == '__main__':
     app.run(debug=True)
